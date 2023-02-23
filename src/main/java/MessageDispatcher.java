dd71dc4f7517c7a117b5a64518b823368f2d5562
kiwi <41132322+whs146@users.noreply.github.com>
2023-02-23 14:59:15 -0800

    update for more details

--- src/main/java/MessageDispatcher.java
+++ src/main/java/MessageDispatcher.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class MessageDispatcher {
+
+import com.rabbitmq.client.*;
+
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class MessageDispatcher implements Runnable {
+
+
+    private Connection connection;
+
+
+
+    public MessageDispatcher(Connection connection) {
+
+        this.connection = connection;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Channel channel = connection.createChannel();
+            channel.basicConsume("direct", true, new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, "UTF-8");
+                    ConcurrentHashMap<String, int[]> map = Consumer.map;
+                    int index = message.indexOf("swiper");
+                    int index2 = message.indexOf("swipee");
+                    String swiper = message.substring(index + 9, index2 - 3);
+                    int index4 = message.indexOf("leftOrRight");
+                    String direction = message.charAt(index4 + 14) == 'r' ? "right" : "left";
+                    if (!map.containsKey(swiper)) {
+                        if (direction.equals("left")) {
+                            map.put(swiper, new int[]{1, 0});
+                        } else {
+                            map.put(swiper, new int[]{0, 1});
+                        }
+                    } else {
+                        if (direction.equals("left")) {
+                            map.get(swiper)[0] += 1;
+                        } else {
+                            map.get(swiper)[1] += 1;
+                        }
+
+                    }
+
+                }
+
+            });
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
+
+
